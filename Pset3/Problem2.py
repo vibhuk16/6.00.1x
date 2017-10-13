95069f19feeb6e190b453cc7b8c08b1a39766165
Gustavo Reyes <20102656@aloe.ulima.edu.pe>
2017-10-14 08:52:08 +0900

    Pset3Problem2.py

---
+++ Pset3/Problem2.py
@@ -0,0 +1,22 @@
+ """
+ Implement the function getGuessedWord that takes in two parameters 
+ A string, secretWord, and a list of letters, lettersGuessed. 
+ This function returns a string that is comprised of letters and underscores, 
+ based on what letters in lettersGuessed are in secretWord
+ """
+ 
+def getGuessedWord(secretWord, lettersGuessed):
+  """
+  secretWord: String, the word the user is guessing
+  lettersGuessed: list, what letters have been guessed so far
+  returns: String, comprised of letters and underscores that represents
+           what letters in secretWord have been guessed so far.
+  """
+  def getGuessedWord(secretWord, lettersGuessed):
+    guessedWord = ""
+    for word in secretWord:
+        if word not in lettersGuessed:
+            guessedWord += "_"
+        else:
+            guessedWord += word
+    return guessedWord
