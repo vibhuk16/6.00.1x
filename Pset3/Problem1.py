15bbc2dc24d78ebf018fe23e5c786fa297bff678
Gustavo Reyes <20102656@aloe.ulima.edu.pe>
2017-10-13 16:25:56 +0900

    Pset3Problem1.py

---
+++ Pset3/Problem1.py
@@ -0,0 +1,19 @@
+ """
+ Implement the function isWordGuessed that takes in two parameters 
+ A string, secretWord, and a list of letters, lettersGuessed. 
+ This function returns a boolean - True if secretWord has been guessed 
+ and False otherwise.
+ """
+ 
+ def isWordGuessed(secretWord, lettersGuessed):
+    """
+    secretWord: String, the word the user is guessing
+    lettersGuessed: List, what letters have been guessed so far
+    returns: Boolean, True if all the letters of secretWord are in lettersGuessed;
+             False otherwise
+    """
+    
+    for word in secretWord:
+        if word not in lettersGuessed:
+            return False
+    return True
