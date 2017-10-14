4e0b64cface56e8eea913a62b8adbecef7bdd8fe
Gustavo Reyes <20102656@aloe.ulima.edu.pe>
2017-10-14 09:25:25 +0900

    Pset3Problem3

---
+++ Pset3/Problem3.py
@@ -0,0 +1,21 @@
+ """
+ implement the function getAvailableLetters that takes in one parameter 
+ A list of letters, lettersGuessed This function returns a string that is comprised 
+ of lowercase English letters all lowercase English letters that are not in lettersGuessed
+ """
+ 
+import string
+def getAvailableLetters(lettersGuessed):
+    """
+    lettersGuessed: List, what letters have been guessed so far
+    returns: String, comprised of letters that represents what letters have not
+             yet been guessed.
+    """
+    
+    allLetters = string.ascii_lowercase
+    while lettersGuessed != []:
+        for word in lettersGuessed:
+            endLetters = "".join(allLetters.rsplit(word))
+            allLetters = endLetters
+        return endLetters
+    return allLetters
